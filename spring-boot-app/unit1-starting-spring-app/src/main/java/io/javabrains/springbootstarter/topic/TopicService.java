06fe4e97fe1a74629eb075ad20341cba1518fe31
DevMubeenBhatti <mubeen.bhatti@systemsltd.com>
2022-02-25 19:06:33 +0500

    Unit 1 Strating a spring boot app => list and add API done

--- spring-boot-app/unit1-starting-spring-app/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ spring-boot-app/unit1-starting-spring-app/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,20 +1,24 @@
 package io.javabrains.springbootstarter.topic;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics =new ArrayList<> (Arrays.asList(
 			new Topic("Spring", "Spring Framwork", "Spring Framework Description"),
 			new Topic("java","Spring Framwork", "Spring Framework Description"),
 			new Topic("javaScript", "Spring Framwork", "Spring Framework Description")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
 	}
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
